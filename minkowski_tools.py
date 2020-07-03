4bf7df50952502630e13ba365454f07c67dd2bbc
mwcotton <22508607+mwcotton@users.noreply.github.com>
2020-07-03 16:09:51 +0100

    Began looking at properties of paths

--- minkowski_tools.py
+++ minkowski_tools.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy as sp
+import scipy.interpolate
 import matplotlib.pyplot as plt
 
 def djikstre(connection_mat):
@@ -18,7 +19,7 @@ def djikstre(connection_mat):
     dist[n-2] = 0.0
     
     while(len(Q)>0):
-        
+
         min_dist = min([dist[key] for key in Q])
         u = [key for key in Q if dist[key] == min_dist][0]
         Q.remove(u)
@@ -75,10 +76,34 @@ def plot_points_simple(ax, points, paths=[], path_labels=[]):
 
     return ax
 
+
+def plot_path_points(ax, points=[], paths=[], path_labels=[]):
+    """
+    Basic plotting function to visaulise paths, without requiring the points.
+    Plots on the 'ax' axis.
+    """
+    cols = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
+    
+    # if points:
+    #     ax.scatter(*points, alpha=0.1, c='k')
+
+#     add functionality to ignore labels
+    for path, label, col in zip(paths, path_labels, cols):
+        ax.plot(*path, alpha=.8, c=col, label=label)
+        ax.scatter(*path, alpha=.6, c=col)
+        
+    ax.set(xlim=[0, 1], ylim=[0, 1])
+    ax.set_axis_off()
+    
+    if path_labels:
+        ax.legend()
+
+    return ax
+
 def shortest_path(connections):
 
     n = connections.shape[0]
-    dists, prev = djikstre(connections)
+    _, prev = djikstre(connections)
     u = n-1
 
     path=[]
@@ -89,3 +114,23 @@ def shortest_path(connections):
         path.append(u)
         
     return path
+
+def points_between(points, path1, path2=np.array([[0, 1pa], [0, 1]])):
+    """
+    Returns the coordinates and boolean indices for points between the two paths.
+    Takes the paths as points
+    """
+    f1, f2 = sp.interpolate.interp1d(*path1), sp.interpolate.interp1d(*path2)
+
+    above_path1 = points[1, :] < f1(points[0, :])
+    above_path2 = points[1, :] < f2(points[0, :])
+
+    return np.logical_xor(above_path1, above_path2)
+
+def path_angles(path):
+    """
+    """
+    dxs = path[0, :-1]-path[0, 1:]
+    dys = path[1, :-1]-path[1, 1:]
+    path_angles = np.arctan(dys/dxs)
+    return path_angles
