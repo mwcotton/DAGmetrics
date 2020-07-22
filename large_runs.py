92360db9078ef32cfc39a116e223f786d6f71d0f
mwcotton <22508607+mwcotton@users.noreply.github.com>
2020-07-22 11:26:31 +0100

    Recreating plots

--- large_runs.py
+++ large_runs.py
@@ -1,10 +1,13 @@
 import numpy as np
 import minkowski_tools as mt
 
-savename ='outputs/4000_40bigrange.txt'
+savename ='outputs/quick_run.txt'
 
-n = 4000
-ps = np.arange(.2, 3.01, 0.01)
-ns = [n]*len(ps)
-rs = mt.norm_kernel_2D(ps, 40/n)
-mt.separate_simulations(ns, ps, rs, savename)
+for _ in range(10):
+    # ns = np.arange(200, 10200, 200)
+    ps = np.arange(0.4, 3, 0.05)
+    n = 1000
+    ns = [n]*len(ps)
+    # rs = [mt.norm_kernel_2D(p, 5/n) for n, p in zip(ns, ps)]
+    rs = [0.2]*len(ps)
+    mt.separate_simulations(ns, ps, rs, savename)
