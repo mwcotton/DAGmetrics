18e748b3b44bc346d2d2f44dec213154863f94b4
mwcotton <22508607+mwcotton@users.noreply.github.com>
2020-07-27 22:52:13 +0100

    Begining percolation

--- large_runs.py
+++ large_runs.py
@@ -1,13 +1,18 @@
 import numpy as np
 import minkowski_tools as mt
 
-savename ='outputs/quick_run.txt'
 
-for _ in range(10):
-    # ns = np.arange(200, 10200, 200)
-    ps = np.arange(0.4, 3, 0.05)
-    n = 1000
-    ns = [n]*len(ps)
-    # rs = [mt.norm_kernel_2D(p, 5/n) for n, p in zip(ns, ps)]
-    rs = [0.2]*len(ps)
+# for edges in [5, 10, 20, 40]:
+
+savename =f'outputs/dimensionrun.txt'
+
+for p in [0.8, 1.2]:
+
+    ns = np.arange(200, 10200, 200)
+    # ps = np.arange(0.4, 3, 0.05)
+    ps = [p]*len(ns)
+    # n = 4000
+    # ns = [n]*len(ps)
+    rs = [mt.norm_kernel_2D(p, 5/n) for n, p in zip(ns, ps)]
+    # rs = [0.2]*len(ps)
     mt.separate_simulations(ns, ps, rs, savename)
